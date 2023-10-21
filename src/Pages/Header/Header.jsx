125aebd3651929636ff06625b605877eac26bc03
Your NameMomenaKhatunzinia <momena.ewu@gmail.com>
2023-10-21 17:50:23 +0600

    added header

--- src/Pages/Header/Header.jsx
+++ src/Pages/Header/Header.jsx
@@ -5,13 +5,38 @@ const Header = () => {
         <div>
            <div className="carousel carousel-center max-w-full  p-4 space-x-4 bg-neutral rounded-box ">
   <div className="carousel-item">
-    <img src="https://i.ibb.co/TPJ3GKy/anh-nhat-Pd-ALQmf-Eqv-E-unsplash.jpg" className="rounded-box  h-[400px]" />
+    <div className="hero min-h-screen" style={{backgroundImage: 'url(https://i.ibb.co/TPJ3GKy/anh-nhat-Pd-ALQmf-Eqv-E-unsplash.jpg)'}}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+      <p className="mb-5">Discover the pinnacle of innovation with our state-of-the-art gadgets that set new industry standards..</p>
+    </div>
+  </div>
+</div>
   </div> 
   <div className="carousel-item">
-    <img src="https://i.ibb.co/G7sLnM5/erick-cerritos-i5-UV2-Hp-ITYA-unsplash.jpg" className="rounded-box  h-[400px]" />
+   
+    <div className="hero min-h-screen" style={{backgroundImage: 'url(https://i.ibb.co/G7sLnM5/erick-cerritos-i5-UV2-Hp-ITYA-unsplash.jpg)'}}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+      <p className="mb-5">Elevate your daily life with devices that bring entertainment, convenience, and productivity to new heights.</p>
+    </div>
+  </div>
+</div>
   </div> 
   <div className="carousel-item">
-    <img src="https://i.ibb.co/SyPDvwK/tomasz-gawlowski-YDZPdqv3-Fc-A-unsplash.jpg" className="rounded-box  h-[400px]" />
+    <div className="hero min-h-screen" style={{backgroundImage: 'url(https://i.ibb.co/SyPDvwK/tomasz-gawlowski-YDZPdqv3-Fc-A-unsplash.jpg)'}}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+      <p className="mb-5">Get ready for unmatched performance and speed that keeps up with your demands.</p>
+    </div>
+  </div>
+</div>
   </div> 
  
 </div> 
